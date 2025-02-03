a437c4f12dfafe37b25ba573e5d98121499944d8
parshav <parshavkhoche16@gmail.com>
2025-02-03 10:50:07 +0530

    hello..parshav

---
+++ hi.py
@@ -0,0 +1,11 @@
+print("hello parshav")
+# Simple program to calculate factorial
+
+num = int(input("Enter a number: "))
+factorial = 1
+
+for i in range(1, num + 1):
+    factorial *= i
+
+print(f"The factorial of {num} is {factorial}")
+
